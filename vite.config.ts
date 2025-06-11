dc0f623b0168b3c1afed312472f631cc701e9a37
jolan <jolan@jolan.home>
2025-06-11 12:20:50 +0200

    mahan

--- vite.config.ts
+++ vite.config.ts
@@ -40,4 +40,8 @@ window.addEventListener('message', async (message) => {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist', // dossier de sortie
+    emptyOutDir: true, // vide le dossier avant chaque build
+  },
 }));
